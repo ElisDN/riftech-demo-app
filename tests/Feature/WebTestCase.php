5c840dee917bb333d24660871dc5dc9c833bd710
ElisDN <mail@elisdn.ru>
2018-09-23 15:51:55 +0300

    Added tests fixtures

--- tests/Feature/WebTestCase.php
+++ tests/Feature/WebTestCase.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Test\Feature;
 
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Loader;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\ServerRequest;
@@ -35,14 +40,35 @@ class WebTestCase extends TestCase
         return $response;
     }
 
+    protected function loadFixtures(array $fixtures): void
+    {
+        $container = $this->getContainer();
+        $em = $container->get(EntityManagerInterface::class);
+        $loader = new Loader();
+        foreach ($fixtures as $class) {
+            if ($container->has($class)) {
+                $fixture = $container->get($class);
+            } else {
+                $fixture = new $class;
+            }
+            $loader->addFixture($fixture);
+        }
+        $executor = new ORMExecutor($em, new ORMPurger($em));
+        $executor->execute($loader->getFixtures());
+    }
+
     private function app(): Application
     {
-        /** @var \Psr\Container\ContainerInterface $container */
-        $container = require 'config/container.php';
+        $container = $this->getContainer();
         $app = $container->get(Application::class);
         $factory = $container->get(MiddlewareFactory::class);
         (require 'config/pipeline.php')($app, $factory, $container);
         (require 'config/routes.php')($app, $factory, $container);
         return $app;
+    }
+
+    private function getContainer(): ContainerInterface
+    {
+        return require 'config/container.php';
     }
 }
